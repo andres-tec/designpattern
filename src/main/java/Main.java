eb2de2909704aef1bb8a4840f36ad1e0341812c9
andres-tec <andrestecpile97@gmail.com>
2024-11-06 05:49:18 -0600

    java InputStream exercise i/o library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,29 @@
-import beveragesinstance.DarkRoast;
-import beveragesinstance.Espresso;
-import beveragesinstance.HouseBlend;
-import condiments.Mocha;
-import condiments.Soy;
-import condiments.Whip;
-import starbuzzcoffee.Beverage;
-import starbuzzcoffee.Sizes;
+import javaio.LowerCaseInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         System.out.println("*******************WELCOME TO THE DECORATOR PATTERN*******************************");
+        int c ;
+        try {
+            InputStream in =
+                    new LowerCaseInputStream(
+                            new BufferedInputStream(
+                                    new FileInputStream("src/main/resources/test.txt")
+                            )
+                    );
+            while ((c = in.read()) >= 0){
+                System.out.print((char) c);
+            }
 
-        Beverage beverage = new Espresso();
-        beverage.setSize(Sizes.TALL);
-        System.out.println(beverage.getDescription()+ " Size:" +beverage.getSize() + " $" +beverage.cost());
-
-        Beverage beverage1 = new DarkRoast();
-        beverage1.setSize(Sizes.GRANDE);
-        beverage1= new Mocha(beverage1);
-        beverage1= new Mocha(beverage1);
-        beverage1= new Whip(beverage1);
-        System.out.println(beverage1.getDescription() + " Size:" +beverage1.getSize() +" $" + beverage1.cost());
-
-        Beverage beverage2= new HouseBlend();
-        beverage2.setSize(Sizes.VENTI);//.20
-        beverage2 = new Soy(beverage2);//.20
-        beverage2= new Mocha(beverage2);//.20
-        beverage2= new Whip(beverage2);//.20
-        System.out.println(beverage2.getDescription() + " Size:" +beverage2.getSize() + " $"  + beverage2.cost());
-
+            in.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/starbuzzcoffee/Sizes.java
+++
@@ -1,8 +0,0 @@
-package starbuzzcoffee;
-
-public enum Sizes {
-    TALL,
-    GRANDE,
-    VENTI,
-
-}
